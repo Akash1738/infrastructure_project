e4e53a2d66f1a66f32d2dbb41dfb03676d35e846
@kki... <akashbajgire12@gmail.com>
2026-08-01 13:23:22 +0530

    Refactor outputs.tf to add public DNS and fix public IP
    Reorganize output variables in outputs.tf to include public DNS and ensure public IP is defined correctly.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
-output "public_ip" {
-
-  value = aws_instance.server.public_ip
-
+output "instance_id" {
+  value = aws_instance.server.id
 }
 
-output "instance_id" {
+output "public_ip" {
+  value = aws_instance.server.public_ip
+}
 
-  value = aws_instance.server.id
-
+output "public_dns" {
+  value = aws_instance.server.public_dns
 }
