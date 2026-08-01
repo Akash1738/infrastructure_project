ce3229425b5255f4c390be1385dde2ea2c2aec24
@kki... <akashbajgire12@gmail.com>
2026-08-01 13:27:28 +0530

    Reorder aws_instance attributes in ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,11 +1,10 @@
 resource "aws_instance" "server" {
 
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = [aws_security_group.devops_sg.id]
-  key_name               = var.key_name
-
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  key_name                    = var.key_name
+  vpc_security_group_ids       = [aws_security_group.devops_sg.id]
   associate_public_ip_address = true
 
   tags = {
