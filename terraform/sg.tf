700f1ba683a744fd053b24d8756b315e8d7a6748
@kki... <akashbajgire12@gmail.com>
2026-08-01 13:37:42 +0530

    Modify security group for DevOps with new rules
    Updated security group to allow Jenkins traffic and modified port settings.

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,38 +1,75 @@
 resource "aws_security_group" "devops_sg" {
 
-  name        = "DevOps-SG"
-  description = "Allow SSH HTTP and App"
-  vpc_id      = var.vpc_id
+  name = "devops-sg"
+
+  description = "Allow SSH HTTP Jenkins"
+
+  vpc_id = aws_vpc.devops_vpc.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+
+    from_port = 22
+
+    to_port = 22
+
+    protocol = "tcp"
+
     cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+
+    from_port = 80
+
+    to_port = 80
+
+    protocol = "tcp"
+
     cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
+
+    from_port = 8080
+
+    to_port = 8080
+
+    protocol = "tcp"
+
     cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+
+    from_port = 3000
+
+    to_port = 3000
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+
+    from_port = 0
+
+    to_port = 0
+
+    protocol = "-1"
+
     cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   tags = {
+
     Name = "DevOps-SG"
+
   }
+
 }
