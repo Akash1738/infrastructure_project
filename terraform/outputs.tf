28a47c1c969ab3ef84b703a39e932c339eab35d2
@kki... <akashbajgire12@gmail.com>
2026-08-01 13:38:29 +0530

    Refactor outputs in outputs.tf for devops_server

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,23 @@
+output "instance_public_ip" {
+
+  value = aws_instance.devops_server.public_ip
+
+}
+
 output "instance_id" {
-  value = aws_instance.server.id
+
+  value = aws_instance.devops_server.id
+
 }
 
-output "public_ip" {
-  value = aws_instance.server.public_ip
+output "vpc_id" {
+
+  value = aws_vpc.devops_vpc.id
+
 }
 
-output "public_dns" {
-  value = aws_instance.server.public_dns
+output "subnet_id" {
+
+  value = aws_subnet.public_subnet.id
+
 }
