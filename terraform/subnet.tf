42ea58699f36cf9e4df9628d79915c886e09d2ef
@kki... <akashbajgire12@gmail.com>
2026-08-01 13:04:29 +0530

    Update VPC ID reference in public subnet resource

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,9 +1,9 @@
 resource "aws_subnet" "public_subnet" {
 
-  vpc_id                  = vpc-02f7a4c516baee7d2
-  cidr_block              = var.public_subnet_cidr
-  availability_zone       = var.availability_zone
-  map_public_ip_on_launch = true
+vpc_id                  = aws_vpc.devops_vpc.id  
+cidr_block              = var.public_subnet_cidr
+availability_zone       = var.availability_zone
+map_public_ip_on_launch = true
 
   tags = {
     Name = "Public-Subnet"
