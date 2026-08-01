2d96ffe2b0e6e51c31cba687858f38e0683752fc
@kki... <akashbajgire12@gmail.com>
2026-08-01 13:10:27 +0530

    Update VPC and subnet references in route table

--- terraform/rt.tf
+++ terraform/rt.tf
@@ -1,22 +1,21 @@
 resource "aws_route_table" "public_rt" {
 
-  vpc_id = vpc-02f7a4c516baee7d2
+  vpc_id = aws_vpc.devops_vpc.id
 
   route {
 
     cidr_block = "0.0.0.0/0"
 
     gateway_id = aws_internet_gateway.igw.id
+
   }
 
-  tags = {
-    Name = "Public-RT"
-  }
 }
 
-resource "aws_route_table_association" "public_assoc" {
+resource "aws_route_table_association" "rt_association" {
 
-  subnet_id      = subnet-096e5fffa72b98f19
+  subnet_id = aws_subnet.public_subnet.id
 
   route_table_id = aws_route_table.public_rt.id
+
 }
