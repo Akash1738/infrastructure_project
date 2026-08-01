8b66fae2456644422afc305d3a2c132baae30130
@kki... <akashbajgire12@gmail.com>
2026-08-01 13:09:06 +0530

    Format subnet.tf for improved readability

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,11 +1,15 @@
 resource "aws_subnet" "public_subnet" {
 
-vpc_id                  = aws_vpc.devops_vpc.id  
-cidr_block              = var.public_subnet_cidr
-availability_zone       = var.availability_zone
-map_public_ip_on_launch = true
+  vpc_id = aws_vpc.devops_vpc.id
+
+  cidr_block = var.public_subnet_cidr
+
+  availability_zone = var.availability_zone
+
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Public-Subnet"
   }
+
 }
