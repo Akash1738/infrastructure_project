ba01bd3b430ddc504b713708c8da503b3ca809af
@kki... <akashbajgire12@gmail.com>
2026-08-01 13:38:12 +0530

    Update EC2 instance resource configuration

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,13 +1,25 @@
-resource "aws_instance" "server" {
+resource "aws_instance" "devops_server" {
 
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
-  key_name                    = var.key_name
-  vpc_security_group_ids       = [aws_security_group.devops_sg.id]
+  ami = var.ami
+
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.public_subnet.id
+
+  key_name = var.key_name
+
   associate_public_ip_address = true
 
+  vpc_security_group_ids = [
+
+    aws_security_group.devops_sg.id
+
+  ]
+
   tags = {
-    Name = "Terraform-Server"
+
+    Name = "DevOps-Server"
+
   }
+
 }
