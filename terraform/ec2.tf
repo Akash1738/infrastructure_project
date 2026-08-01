f78ad9e9e865b39b0c999bccd7b305b8ecbf5eeb
@kki... <akashbajgire12@gmail.com>
2026-08-01 13:23:00 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,19 +1,14 @@
 resource "aws_instance" "server" {
 
-  ami = var.ami
-
-  instance_type = var.instance_type
-
-  subnet_id = aws_subnet.public_subnet.id
-
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.devops_sg.id]
+  key_name               = var.key_name
 
-  key_name = var.key_name
+  associate_public_ip_address = true
 
   tags = {
-
     Name = "Terraform-Server"
-
   }
-
 }
