17a7e612444f65b8ba0551b0f647654d11a57e3d
@kki... <akashbajgire12@gmail.com>
2026-08-01 13:52:28 +0530

    Refactor subnet resource configuration

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,15 +1,10 @@
 resource "aws_subnet" "public_subnet" {
-
-  vpc_id = aws_vpc.devops_vpc.id
-
-  cidr_block = var.public_subnet_cidr
-
-  availability_zone = var.availability_zone
-
+  vpc_id                  = aws_vpc.devops_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
 
   tags = {
     Name = "public-subnet"
   }
-
 }
